9a01afbdc726cfb30e36833e967150be52e826e0
Suzan Sucro <suzansucro@gmail.com>
2017-03-01 18:16:26 -0800

    Add image minification plugin and gulp tasks. Add font gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
 
 gulp.task('browserSync', function(){
   browserSync.init({
@@ -13,7 +15,6 @@ gulp.task('browserSync', function(){
     },
   })
 });
-
 gulp.task('useref', function(){
   return gulp.src('app/*.html')
     .pipe(useref())
@@ -23,7 +24,6 @@ gulp.task('useref', function(){
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'))
 });
-
 gulp.task('sass', function(){
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass())
@@ -32,7 +32,18 @@ gulp.task('sass', function(){
       stream: true
     }))
 });
-
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+    // cache already minified imgs for speed
+    .pipe(imagemin({
+      interlaced: true
+    }))
+    .pipe(gulp.dest('dist/images'))
+});
+gulp.task('fonts', function(){
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+});
 gulp.task('watch', ['browserSync', 'sass'], function(){
   gulp.watch('app/scss/**/*.scss', ['sass']);
   gulp.watch('app/*.html', browserSync.reload);
