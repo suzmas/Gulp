2602f38c07e762579eb9601b2876655a1c06c89e
Suzan Sucro <suzansucro@gmail.com>
2017-03-10 21:44:41 -0800

    Do some more stuff :-)

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,10 @@
-$('.mdl-navigation__link').click(function(){
-  $(this).addClass("active-link");
+$(document).ready(function() {
+  $('nav a').click(function(){
+  $(this).addClass('active-link');
+  $(this).parent().parent().children('a').not(this).removeClass('active-link');
 });
+});
+
+//broken because the nav element is not the direct parent and the a
+// elements are not the direct children- fix
 console.log("what");
